8851c19e5341ba9d8ff991c3fe5909c14064c307
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-09-08 11:11:16 +0200

    added more dynamic error behavior

--- login.js
+++ login.js
@@ -2,11 +2,19 @@ const form = document.querySelector("#login-form");
 const username = document.querySelector("#username-field")
 const password = document.querySelector('#password-field')
 const loginErrorMsg = document.querySelector("#login-error-message");
+const formField = document.querySelectorAll(".login-form-field")
 
-form.addEventListener("submit", event => {
+
+form.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log(username.value)
     if (!username.value || !password.value) {
      loginErrorMsg.style.visibility = "initial";
     }
+    if (!username.value) {
+       username.style.borderBottom ="1px solid #ce102d";
+    }
+    if (!password.value) {
+      password.style.borderBottom ="1px solid #ce102d";
+    }
 });
+
